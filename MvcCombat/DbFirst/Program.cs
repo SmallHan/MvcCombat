3407b8f0c9234fa659f70d04dded085fe927cf8f
SmallHan <316598703@qq.com>
2017-06-19 15:37:33 +0800

    DbFirst

--- MvcCombat/DbFirst/Program.cs
+++ MvcCombat/DbFirst/Program.cs
@@ -10,6 +10,18 @@ namespace DbFirst
     {
         static void Main(string[] args)
         {
+            EFDbEntities entity = new EFDbEntities();
+            t_customer customer = new t_customer()
+            {
+                Adress="东海五彩金轮",
+                Age=23,
+                UserName="小瀚"
+            };
+            //构造sql
+            entity.t_customer.Add(customer);
+            entity.SaveChanges();
+            Console.WriteLine("ok");
+            Console.ReadKey();
         }
     }
 }
